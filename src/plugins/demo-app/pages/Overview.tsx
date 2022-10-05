eabda93178e24f89f016dfadd2a0f55fdca81545
Kristian Kjerstad <69512295+KristianKjerstad@users.noreply.github.com>
2022-10-05 15:52:32 +0200

    fix: remove deployment things from CI (#1)
    update CI: remove deployment stuff, disable jest and add eslint

--- src/plugins/demo-app/pages/Overview.tsx
+++ src/plugins/demo-app/pages/Overview.tsx
@@ -3,6 +3,7 @@ import {
 } from '@development-framework/dm-core'
 import React, { ReactNode } from 'react'
 
+
 export const Overview = (props: { settings: TDmtSettings }): ReactNode => {
 
   return (
