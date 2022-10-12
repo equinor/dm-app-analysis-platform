a608452fb0fe808f43be3f1cddd943ac559c8edb
kristian <kristian.kjerstad@bouvet.no>
2022-10-12 16:34:06 +0200

    feat: add taks plugins to AP (copy from dmt repo)

--- src/plugins/analysis-platform/index.tsx
+++ src/plugins/analysis-platform/index.tsx
@@ -3,6 +3,9 @@ import { EDmtPluginType } from '@development-framework/dm-core'
 import App from './App'
 import { InspectorView, OperatorView } from './modules/Analysis'
 import { AssetView } from './modules/Asset/AssetView'
+import {EditTask} from "./plugins/task/src/EditTask";
+import {ViewTask} from "./plugins/task/src/ViewTask";
+import {EditInput} from "./plugins/task/src/InputOnly";
 
 export const plugins: any = [
   {
@@ -25,6 +28,25 @@ export const plugins: any = [
     pluginType: EDmtPluginType.UI,
     component: AssetView,
   },
+  {
+    pluginName: 'edit-task',
+    pluginType: EDmtPluginType.UI,
+    component: EditTask,
+  },
+  {
+    pluginName: 'view-task',
+    pluginType: EDmtPluginType.UI,
+    component: ViewTask,
+  },
+  {
+    pluginName: 'edit-task-operator',
+    pluginType: EDmtPluginType.UI,
+    component: EditInput,
+  },
+
+    //TODO now, I have copied EditTask, ViewTask and EditInput from dmt/packages/plugins/task into src/plugins/analysis-platform/plugins.
+    //Question: is it okay to include plugins inside the analysis-portal plugin? the stuff inside dmt/packages/plugins/task is related to BPs from analysis portal, and I think task should not be a plugin in dm-core-pacakges.
+    // the stuff inside analysis-platform/plugins might requires separate packages (for example EditTask requires Lodash)
 ]
 
 export * from './utils/auth'
