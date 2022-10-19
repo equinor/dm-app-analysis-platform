1ea039770494a0beb95e5fef1e63f973544a5af2
kristian <kristian.kjerstad@bouvet.no>
2022-10-19 12:18:48 +0200

    chore: delete demo app code and data

--- src/plugins/demo-app/pages/Overview.tsx
+++
@@ -1,6 +0,0 @@
-import { TDmtSettings } from '@development-framework/dm-core'
-import React, { ReactNode } from 'react'
-
-export const Overview = (props: { settings: TDmtSettings }): ReactNode => {
-  return <>Overview</>
-}
