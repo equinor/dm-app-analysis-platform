fd155e9c66de0abdd99c445627f8a8ea15fa1ca3
kristian <kristian.kjerstad@bouvet.no>
2022-10-19 12:17:48 +0200

    fix: use new settings structure

--- src/plugins/demo-app/App.tsx
+++ src/plugins/demo-app/App.tsx
@@ -1,22 +1,25 @@
 // @ts-nocheck
 
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route } from 'react-router-dom'
 import { Layout } from 'antd'
 import Routes from './Routes'
-import { TApp, TLayout } from '@development-framework/dm-core'
+import {
+  ApplicationContext,
+  TApp,
+  TLayout,
+} from '@development-framework/dm-core'
 import Content from './layout/Content'
 import Menu from './layout/Menu'
 import { Header } from './layout/Header'
 
 const MainLayout = (props: TLayout) => {
-  const { content, settings, allApps } = props
+  const { content, settings } = props
   return (
     <>
       <Header
-        appName={settings.label}
+        appName={settings.label ?? settings.name}
         urlPath={settings.urlPath}
-        allApps={allApps}
       />
       <Layout>
         <Menu appRootPath={settings.urlPath} />
@@ -26,25 +29,24 @@ const MainLayout = (props: TLayout) => {
   )
 }
 
-export default (props: TApp): JSX.Element => {
-  const { settings, applications } = props
+const App = (props: TApp): JSX.Element => {
+  const settings = useContext(ApplicationContext)
+  const urlPath = settings.urlPath ? `/${settings.urlPath}` : ''
 
   return (
     <>
       {Routes.map((route) => (
         <Route
           exact
-          path={`/${settings.urlPath}${route.path}`}
+          path={`${urlPath}/${route.path}`}
           key={route.path}
           render={() => (
-            <MainLayout
-              allApps={applications}
-              content={route.content}
-              settings={settings}
-            />
+            <MainLayout content={route.content} settings={settings} />
           )}
         />
       ))}
     </>
   )
 }
+
+export default App
